0a5a6ac9d3b64cb944c76b5f5b1c5a1c260b053e
Melyssa Pereira <meelyssa2411@gmail.com>
2025-06-02 01:18:33 +0000

    CORREÇÃO: addEventListener escrito errado no botão de fechar menu

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const body = document.querySelector("body")
 const nav = document.querySelector("header .mobile")
 
 botaoAbrir.addEventListener('click', abrirMenu)
-botaoFechar.addEventListener.add('click', fecharMenu)
+botaoFechar.addEventListener('click', fecharMenu)
 
 function abrirMenu() {
     body.classList.add("escurecer")
